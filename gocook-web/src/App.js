a4058550fdc5309d7936e20a130654789d320761
Jack Schofield <jjmschofield@googlemail.com>
2018-08-27 10:22:25 +0100

    Giving body some spacing

--- gocook-web/src/App.js
+++ gocook-web/src/App.js
@@ -1,6 +1,7 @@
 // @flow
 import React, {Component} from 'react';
 import {Switch, Route, Redirect} from 'react-router';
+import {withStyles} from '@material-ui/core/styles';
 
 import AppToolbar from './components/app/AppToolbar';
 
@@ -9,22 +10,32 @@ import TodayView from './routes/today/TodayView';
 import RecipesView from './routes/recipes/RecipesView';
 import PlansView from './routes/plans/PlansView';
 
-type Props = {};
+type Props = {
+    classes: any,
+};
+
+const styles = {
+    view: {
+        padding: "0 24px"
+    }
+};
 
 class App extends Component<Props> {
     render() {
         return (
             <React.Fragment>
                 <AppToolbar routes={Object.values(NAV_ROUTES)}/>
-                <Switch>
-                    <Route path={NAV_ROUTES.TODAY.path} component={TodayView}/>
-                    <Route path={NAV_ROUTES.RECIPES.path} component={RecipesView}/>
-                    <Route path={NAV_ROUTES.PLANS.path} component={PlansView}/>
-                    <Redirect to={NAV_ROUTES.TODAY.path} />
-                </Switch>
+                <div className={this.props.classes.view}>
+                    <Switch>
+                        <Route path={NAV_ROUTES.TODAY.path} component={TodayView}/>
+                        <Route path={NAV_ROUTES.RECIPES.path} component={RecipesView}/>
+                        <Route path={NAV_ROUTES.PLANS.path} component={PlansView}/>
+                        <Redirect to={NAV_ROUTES.TODAY.path}/>
+                    </Switch>
+                </div>
             </React.Fragment>
         );
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
