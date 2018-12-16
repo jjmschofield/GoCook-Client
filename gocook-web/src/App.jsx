82461a6f1275c50eeede8804508ef8f43c7a2b9f
Jack Schofield <jjmschofield@googlemail.com>
2018-12-16 01:59:37 +0000

    Refactoring to improve abstraction

---
+++ gocook-web/src/App.jsx
@@ -0,0 +1,26 @@
+// @flow
+import React, { Component } from 'react';
+import { Auth } from './lib/auth'
+
+import AppToolbar from './components/app/AppToolbar';
+import NAV_ROUTES from './routes/NAV_ROUTES';
+import MainView from "./components/app/MainView";
+
+type Props = {
+    auth: Auth,
+};
+
+class App extends Component<Props> {
+    render() {
+        const { auth } = this.props;
+
+        return (
+            <React.Fragment>
+                {auth.isAuthenticated() && <AppToolbar routes={Object.values(NAV_ROUTES)}/>}
+                <MainView auth={auth}/>
+            </React.Fragment>
+        )
+    }
+}
+
+export default App;
--- gocook-web/src/index.js
+++ gocook-web/src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import JssProvider from 'react-jss/lib/JssProvider';
-import {create} from 'jss';
-import {createGenerateClassName, jssPreset} from '@material-ui/core/styles';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import { Auth } from "./lib/auth";
 
 const generateClassName = createGenerateClassName();
 const jss = create(jssPreset());
 jss.options.insertionPoint = 'jss-insertion-point';
 
+const auth = new Auth();
+
 ReactDOM.render(
     <JssProvider jss={jss} generateClassName={generateClassName}>
         <BrowserRouter>
-            <App/>
+            <App auth={auth}/>
         </BrowserRouter>
     </JssProvider>,
     document.getElementById('root')
