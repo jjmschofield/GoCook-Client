81138d1f394b92a26e3f9850534b2cc0ffc3728a
Jack Schofield <jjmschofield@googlemail.com>
2018-12-16 23:04:21 +0000

    Creating a nav button component and connecting to router

--- gocook-web/src/components/app/MobileMainNav.jsx
+++ gocook-web/src/components/app/MobileMainNav.jsx
@@ -1,18 +1,15 @@
 // @flow
 import React, { Component } from 'react';
-
-import { withStyles } from '@material-ui/core/styles';
-
-
 import { withRouter } from "react-router";
-
-import Button from '@material-ui/core/Button';
-import SaveIcon from '@material-ui/icons/Save';
+import { withStyles } from '@material-ui/core/styles';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import CreateIcon from '@material-ui/icons/Create';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PersonIcon from '@material-ui/icons/Person';
+
+import MobileNavButton from './MobileNavButton';
+import { ROUTES } from '../../routes';
 
 type Props = {
     classes: any,
@@ -22,50 +19,47 @@ const styles = {
     toolbar: {
         flexGrow: 1,
         display: 'flex',
-        position:'absolute',
-        left:0,
-        bottom:0,
-        right:0,
+        position: 'absolute',
+        left: 0,
+        bottom: 0,
+        right: 0,
     },
-    button: {
-        flexGrow: 1,
-        display: 'flex',
-        borderRadius: '0px'
-    }
 };
 
 class MobileMainNav extends Component<Props> {
-    state = {
-        activeTabIndex: 0
-    };
-
-    handleTabChange = (event: Event, selectedTabIndex: number) => {
-        this.setState({
-            activeTabIndex: selectedTabIndex
-        });
-        this.props.history.push(`/browse/${selectedTabIndex}`);
-    };
+    handleClick(route) {
+        this.props.history.push(route);
+    }
 
     render() {
         const { classes } = this.props;
 
         return (
             <div className={classes.toolbar}>
-                <Button className={classes.button} size="small">
-                    <DashboardIcon />
-                </Button>
-                <Button className={classes.button} size="small">
-                    <FavoriteBorderIcon />
-                </Button>
-                <Button className={classes.button} size="small">
-                    <CreateIcon />
-                </Button>
-                <Button className={classes.button} size="small">
-                    <ShoppingCartIcon />
-                </Button>
-                <Button className={classes.button} size="small">
-                    <PersonIcon />
-                </Button>
+                <MobileNavButton route={ROUTES.BROWSE}
+                                 icon={<DashboardIcon/>}
+                                 onClick={this.handleClick.bind(this)}
+                                 className={classes.button}/>
+
+                <MobileNavButton route={ROUTES.FAVORITES}
+                                 icon={<FavoriteBorderIcon/>}
+                                 onClick={this.handleClick.bind(this)}
+                                 className={classes.button}/>
+
+                <MobileNavButton route={ROUTES.AUTHOR}
+                                 icon={<CreateIcon/>}
+                                 onClick={this.handleClick.bind(this)}
+                                 className={classes.button}/>
+
+                <MobileNavButton route={ROUTES.SHOPPING}
+                                 icon={<ShoppingCartIcon/>}
+                                 onClick={this.handleClick.bind(this)}
+                                 className={classes.button}/>
+
+                <MobileNavButton route={ROUTES.USER_PROFILE}
+                                 icon={<PersonIcon/>}
+                                 onClick={this.handleClick.bind(this)}
+                                 className={classes.button}/>
             </div>
         );
     };
